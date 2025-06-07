3993c525963d2b9c42609a916685ce87c75feb21
liuwei <1097408921@qq.com>
2025-06-07 20:31:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Button from './components/Button/Button.vue'
+</script>
+
+<template>
+  <h2>按钮</h2>
+  <Button type="primary">按钮</Button>
+  <Button type="success">按钮</Button>
+  <Button type="warning">按钮</Button>
+  <Button type="danger">按钮</Button>
+  <Button type="info">按钮</Button>
+  
+  <Button type="primary" size="large">按钮</Button>
+  <Button type="primary" size="small">按钮</Button>
+  <Button type="primary" round>按钮</Button>
+  <Button type="primary" circle>按钮</Button>
+  <Button type="primary" plain>按钮</Button>
+  <Button type="primary" disabled>按钮</Button>
+  <Button type="primary" loading>按钮</Button>
+</template>
+
+<style scoped></style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    ref="_ref"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+    :disabled="disabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+
+defineOptions({
+  name: 'VkButton',
+})
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+})
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref,
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
