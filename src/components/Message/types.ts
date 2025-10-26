aa1fe6ed235adeae83b458ad6c66cd325bacff18
liuwei <1097408921@qq.com>
2025-10-26 20:38:12 +0800

    feat: Message.vue组件测试

--- src/components/Message/types.ts
+++ src/components/Message/types.ts
@@ -1,18 +1,20 @@
 import type { VNode, ComponentInternalInstance } from 'vue'
-
 export interface MessageProps {
   message?: string | VNode
   duration?: number
   showClose?: boolean
-  type?: 'info' | 'success' | 'warning' | 'error'
-  onDestroy?: () => void
+  type?: 'success' | 'info' | 'warning' | 'danger'
+  onDestroy: () => void
+  id: string
+  zIndex: number
   offset?: number
-  id?: string
+  transitionName?: string
 }
-
 export interface MessageContext {
   id: string
   vnode: VNode
   vm: ComponentInternalInstance
   props: MessageProps
+  destroy: () => void
 }
+export type CreateMessageProps = Omit<MessageProps, 'onDestroy' | 'id' | 'zIndex'>
