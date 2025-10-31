16fae8553a4bf93ddef1e18393f9565d034ab453
liuwei <1097408921@qq.com>
2025-10-31 12:29:36 +0800

    feat: Input组件

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { InputProps } from './types.ts'
+
+defineOptions({
+  name: 'VKInput'
+})
+
+withDefaults(defineProps<InputProps>(), { type: 'text' })
+</script>
+
+<template>
+  <div
+    class="vk-input"
+    :class="{
+      [`vk-input--${type}`]: type,
+      [`vk-input--${size}`]: size,
+      'is-disabled': disabled,
+      'is-prepend': $slots.prepend,
+      'is-append': $slots.append,
+      'is-prefix': $slots.prefix,
+      'is-suffix': $slots.suffix
+    }"
+  ></div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Input/types.ts
@@ -0,0 +1,7 @@
+export interface InputProps {
+  type: string
+  size?: 'small' | 'large'
+  disabled?: boolean
+  clearable?: boolean
+  showPassword?: boolean
+}
