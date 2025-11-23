23b16469c6fba11c5e6b48f3ec185356b6fc546f
liuwei <1097408921@qq.com>
2025-11-23 10:23:53 +0800

    refactor: Input.vue

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -20,7 +20,37 @@ withDefaults(defineProps<InputProps>(), { type: 'text' })
       'is-prefix': $slots.prefix,
       'is-suffix': $slots.suffix
     }"
-  ></div>
+  >
+    <!--  input  -->
+    <template v-if="type !== 'textarea'">
+      <!--  prepend slot  -->
+      <div v-if="$slots.prepend" class="vk-input__prepend">
+        <slot name="prepend"></slot>
+      </div>
+
+      <div class="vk-input__wrapper">
+        <!--  prefix slot  -->
+        <span v-if="$slots.prefix" class="vk-input__prefix">
+          <slot name="append"></slot>
+        </span>
+        <input class="vk-input__inner" :type="type" :disabled="disabled" />
+        <!--  suffix slot  -->
+        <span v-if="$slots.suffix" class="vk-input__suffix">
+          <slot name="append"></slot>
+        </span>
+      </div>
+
+      <!--  append slot  -->
+      <div v-if="$slots.append" class="vk-input__prepend">
+        <slot name="append"></slot>
+      </div>
+    </template>
+
+    <!--  textarea  -->
+    <template v-else>
+      <textarea class="vk-textarea__wrapper" :disabled="disabled" />
+    </template>
+  </div>
 </template>
 
 <style scoped></style>
